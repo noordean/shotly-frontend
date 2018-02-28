cffd6b6b14a917caac32d298204688a7c536a5c9
noordean <ebroyeem90@gmail.com>
2018-02-28 18:22:32 +0100

    chore(setup): setup project
    - create project setup
    
    [Finishes #155590054]

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.js'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
