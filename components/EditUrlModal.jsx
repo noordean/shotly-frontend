23f9e7dce568b670c5a0533405965c1859c702a0
noordean <ebroyeem90@gmail.com>
2018-03-07 17:27:49 +0100

    feat(editModal): create url editing modal
    - create a modal for editing url
    
    [Delivers #155736323]

--- components/EditUrlModal.jsx
+++ components/EditUrlModal.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 import Input from './Input.jsx';
 import Button from './Button.jsx';
 
-export default class Contact extends React.Component {
+export default class EditUrlModal extends React.Component {
 
   render() {
     return (
-        <div className="modal fade" id="contactModal" tabIndex="-1" role="dialog" aria-labelledby="contactModalLabel" aria-hidden="true">
+        <div className="modal fade" id="editModal" tabIndex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
           <div className="modal-dialog" role="document">
             <div className="modal-content">
               <div className="modal-header">
-                  <h5 className="modal-title" id="contactModalLabel">Contact us</h5>
+                  <h5 className="modal-title" id="editModalLabel">Edit URL</h5>
                   <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                   </button>
@@ -19,21 +19,16 @@ export default class Contact extends React.Component {
               <div className="modal-body">
                 <form>
                   <div className="form-group">
-                    <label>Name</label>
                     <Input
                       inputType="text"
-                      inputPlaceHolder="Enter your name"
+                      inputPlaceHolder="https://current-url.com"
                     />
-                  </div>
-                  <div className="form-group">
-                    <label>Message</label>
-                    <textarea className="form-control" placeholder="Enter your message"></textarea>
                   </div>
                 </form>
               </div>
               <div className="modal-footer">
                 <button type="button" className="btn btn-danger modal-close-btn" data-dismiss="modal">Close</button>
-                <button type="button" className="btn btn-success modal-send-btn">Send</button>
+                <button type="button" className="btn btn-success modal-send-btn">Save</button>
               </div>
             </div>
           </div>
